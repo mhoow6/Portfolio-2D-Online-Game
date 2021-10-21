8db5115a4edf51ff4de50a4ad5adc9b403bca4e8
Wooyoung Jang <mhoow6@gmail.com>
2021-10-22 02:14:17 +0900

    경로 수정

--- Assets/Scripts/Paths.cs
+++ Assets/Scripts/Paths.cs
@@ -3,5 +3,5 @@
 public struct Paths
 {
     public readonly static string Map_Prefabs = "Prefabs/Map";
-    public readonly static string Map_Collision = "Assets/Resources/Map";
+    public readonly static string Map_Collision = "Map";
 }
